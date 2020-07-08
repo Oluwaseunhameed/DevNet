e98664e9012b4571aed5723d703e19832928d787
Oluwaseunhameed <hameedoluwaseun@gmail.com>
2020-07-08 08:09:23 +0100

    update on gitignore file

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express")
+const connectDB = require('./config/db')
+
 const app = express()
 
-const PORT = process.env.PORT || 5000
+// Connect Database
+connectDB()
 
 app.get("/", (req, res) => res.send("API running!"))
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
